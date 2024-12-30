30a754a64b9ce294aee1b246d93297acd8820793
Ciruh <marieianbelo@gmail.com>
2024-12-30 21:52:53 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+#     
+# 1. python
+# 2. R
+# 3. pyspark
+# 4. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Pandas
+# 2. Numpy
+# 3. Matplotlib
+# 4. Scikit
+
+# Data Science Tools and Ecosystem
+
+# |Data Science Tools|
+# |-----|
+# |Tensor Flow|
+# |Jupyter Notebooks/lab|
+# |Git|
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# Objectives
+# - Learn basics of Python
+# - Learn basics of R language
+# - Learn how to git version control works
+# - Brief intro into Github
+
+# ## Author
+# Ian
+
+# In[ ]:
+
+
+
+
